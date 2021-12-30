79d7713f684a4828c8ffe55aee3b2f26ffdf928b
dharmeshvyas <dharmeshvyas9099@gmail.com>
2021-12-30 16:05:30 +0530

    Discribe about MaterialApp,Scaffold, and inside the Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,57 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(homapp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class homapp extends StatelessWidget {
   @override
+  //the build method come from StatelessWidget
   Widget build(BuildContext context) {
+    //inside build method we can declear no of Widgets
+
+    //the MaterialApp is pre defined class in flutter, it is main or core component of flutter
+    //using MaterialApp we can access all other comoponet and Widget from flutter sdk
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      //home property describe that should display into mobile screen
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+      //Scaffold is class in flutter which provide many widget or we can say as API like Drawer  EX:AppBar ,SnackBar,BottomNAvigationBar,FloatingActionBar,
+      home: Scaffold(
+        //appBar property it take AppBar()Widget for argument
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        //AppBar() Widget is Used to create or modify for appbar
+        appBar: AppBar(
+          //the first Propery of AppBar is Title
+          //title property used to give title(text) of bar.
+          //text Widget used to add text on the Screen and the first argument is String
+          title: Text("AppBar of App"),
+          //centerTitle property used to align text center in appbar and it take bool value.
+          centerTitle: true,
         ),
+        //body property is main screen of our app after appbar like bodyTag
+        body: Text("This is body of the app"),
+
+        //floatingActionButton is circuler icon button that hover over the content or in the body ant it show all time
+
+        //we can create floating button using FloatingActionButton() Widget.
+        // FloatingActionButton take least 2 parameter child and onPressed.
+        floatingActionButton: FloatingActionButton(
+          //the child property used for adding label icon or more.
+
+          //Icon Widget is used to add icon it Icon and for access icon add name of icon which provided into flutter
+          child: Icon(Icons.message),
+
+          // onPressed property used for click action on the button
+          onPressed: () {},
+          backgroundColor: Colors.lightBlue,
+        ),
+        //bottomNavigationBar it's like appbar we also add add bar into bottom.
+        //bottomNavigationBar has multiple items and can use text,lable,icon and both
+
+        //the bottomNavigationBar is generally created for displaying messages as well as providing page spacific shortcu action
+        bottomNavigationBar: Text("Helo"),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+    throw UnimplementedError();
   }
 }
